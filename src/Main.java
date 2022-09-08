2a98203eab5c6c35a13ccdf6136ef8bfcb0737ba
ElmiraPletneva <elmirapletneva@yandex.ru>
2022-09-08 19:33:53 +0300

    Condition added

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int score = 500; // сумма на счете
-        int amount = 1500; //пополнил на сумму
+        int amount = 1500; //пополнил на сумму 1500
+        int bonus = amount / 100;
         if (amount > 1000) {
-            System.out.println(amount / 100); // кол-во бонусов
-            System.out.println(amount / 100 + amount + score); // сумма на счете всего с учетом бонусов
-
-            int replenishment = 300; // пополнил на сумму
-            if (replenishment > 1000) {
-                System.out.println(replenishment / 100);
-            } // кол-во бонусов
-
-            System.out.println(replenishment + score); // итого на счете с учетом будет бонус или нет
+            System.out.println(amount + bonus + score + " рублей на счету у клиента");
+        } // в случае если сумма пополнения больше 1000
+        else {
+            System.out.println(score + +amount + " рублей на счету у клиента");
+        } // в случае если сумма пополнения меньше 1000
 
 
-        }
-}}
+    }
+}
